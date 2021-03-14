8a3ce6db8b888b795d434224d9857d6ac5ddc640
Victor Vieira <victorvieira89@gmail.com>
2021-03-14 11:47:52 -0300

    feat(home): add home screen

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,28 @@
-import Menu from "./Menu";
+import styled from "styled-components";
 
-export { Menu };
+import Menu from "./Menu";
+import SocialBar from "./SocialBar";
+
+export const Headline = styled.h1`
+  font-family: "Montserrat", sans-serif;
+  font-weight: 900;
+  font-size: 42px;
+  line-height: 100%;
+
+  color: #00edd4;
+`;
+
+export const CodeText = styled.p`
+  font-family: "Roboto Mono", monospaced;
+  font-size: 12px;
+  color: #7957f2;
+`;
+
+export const CodeBlock = styled(CodeText)`
+  padding: 15px 30px;
+  background-color: #24074a;
+  color: #4907a2;
+  border-radius: 10px;
+`;
+
+export { Menu, SocialBar };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,13 @@
 import React from "react";
+import Typist from "react-typist";
 
-import { Menu } from "../../components";
+import {
+  Menu,
+  SocialBar,
+  Headline,
+  CodeText,
+  CodeBlock,
+} from "../../components";
 
 import "./styles.sass";
 
@@ -8,6 +15,41 @@ const Home = () => {
   return (
     <div className="home">
       <Menu />
+      <div className="container">
+        <SocialBar />
+        <Headline>
+          Howdy.
+          <br />
+          I'm Victor
+        </Headline>
+        <Typist
+          cursor={{
+            show: false,
+          }}
+        >
+          <CodeText>$ {">"} vim skills.js</CodeText>
+          <Typist.Delay ms={500} />
+          <CodeBlock>
+            {"//"} front-end developer
+            <br />
+            skills.dev = [<br />
+            ”react.js",
+            <br />
+            ”vue.js”
+            <br />
+            ]<br />
+            <br />
+            {"//"} designer
+            <br />
+            skills.des [<br />
+            ”ux”,
+            <br />
+            ”motion”
+            <br />
+            ]<br />
+          </CodeBlock>
+        </Typist>
+      </div>
     </div>
   );
 };
